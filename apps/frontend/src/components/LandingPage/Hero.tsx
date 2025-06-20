f324a373a573a2516311f9e8a8088d87eabda66c
T4910 <ttaiwo4910@gmail.com>
2025-06-20 11:42:16 +0100

    🚨 fix: error is in req lib

--- apps/frontend/src/components/LandingPage/Hero.tsx
+++ apps/frontend/src/components/LandingPage/Hero.tsx
@@ -1,6 +1,5 @@
 import heroImg from '@/assets/images/hero.png'
-// import type { Patients } from '@shared/index'
-import type { User } from '@shared/index'
+// import type { User } from '@shared/index'
 
 export default function Hero() {
   return (
