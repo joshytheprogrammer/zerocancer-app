37e484dae928bb16877ebaaed3a03421ee844f01
T4910 <ttaiwo4910@gmail.com>
2025-06-20 11:48:52 +0100

    🚨 fix: error is in req lib

--- apps/frontend/src/services/authService.ts
+++ apps/frontend/src/services/authService.ts
@@ -1,59 +1,59 @@
-import request from '@/lib/request'
-import * as endpoints from '@/services/endpoints'
-import * as t from '@/services/types'
+// import request from '@/lib/request'
+// import * as endpoints from '@/services/endpoints'
+// import * as t from '@/services/types'
 
-export const register = (params: t.TRegisterParams) => {
-  return request.post(endpoints.register(), params)
-}
+// export const register = (params: t.TRegisterParams) => {
+//   return request.post(endpoints.register(), params)
+// }
 
-type TLoginResponse = { access: string; refresh: string }
-export const login = (params: t.TLoginParams): Promise<TLoginResponse> => {
-  return request.post(endpoints.login(), params)
-}
+// type TLoginResponse = { access: string; refresh: string }
+// export const login = (params: t.TLoginParams): Promise<TLoginResponse> => {
+//   return request.post(endpoints.login(), params)
+// }
 
-// get user
-export const getCurrentUser = () => {
-  return request.get<t.TUser>(endpoints.user())
-}
+// // get user
+// export const getCurrentUser = () => {
+//   return request.get<t.TUser>(endpoints.user())
+// }
 
-// logout
-export const logout = () => {
-  return request.post(endpoints.logout())
-}
+// // logout
+// export const logout = () => {
+//   return request.post(endpoints.logout())
+// }
 
-// request password reest
-export const requestPasswordReset = (params: t.TRequestPasswordResetParams) => {
-  return request.post(endpoints.requestPasswordReset(), params)
-}
+// // request password reest
+// export const requestPasswordReset = (params: t.TRequestPasswordResetParams) => {
+//   return request.post(endpoints.requestPasswordReset(), params)
+// }
 
-// reset password
-export const resetPassword = ({
-  uidb64,
-  token,
-  ...params
-}: t.TResetPasswordParams) => {
-  return request.post(endpoints.resetPassword(uidb64, token), {
-    uidb64,
-    token,
-    ...params,
-  })
-}
+// // reset password
+// export const resetPassword = ({
+//   uidb64,
+//   token,
+//   ...params
+// }: t.TResetPasswordParams) => {
+//   return request.post(endpoints.resetPassword(uidb64, token), {
+//     uidb64,
+//     token,
+//     ...params,
+//   })
+// }
 
-// confirm email
-export const confirmEmail = (params: t.TConfirmEmailParams) => {
-  return request.post(endpoints.verifyEmail(), params)
-}
+// // confirm email
+// export const confirmEmail = (params: t.TConfirmEmailParams) => {
+//   return request.post(endpoints.verifyEmail(), params)
+// }
 
-// resend confirmation email
-export const resendConfirmEmail = (params: t.TResendConfirmEmailParams) => {
-  return request.post(endpoints.resendVerifyEmail(), params)
-}
+// // resend confirmation email
+// export const resendConfirmEmail = (params: t.TResendConfirmEmailParams) => {
+//   return request.post(endpoints.resendVerifyEmail(), params)
+// }
 
-// 2fa setup
+// // 2fa setup
 
-// password reset
+// // password reset
 
-// confirm email
+// // confirm email
 
-// 2fa setup
-// 2fa verify
+// // 2fa setup
+// // 2fa verify
